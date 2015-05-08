54da7e6c174c7ffd677ae1dfa763fa7fdb021dd7
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-05-08 20:57:13 +0530

    Better arg parsing

--- mytime.c
+++ mytime.c
@@ -16,8 +16,7 @@
 
 typedef void (*sighandler)(int);
 
-struct times_val_t
-{
+struct times_val_t {
 	double utime;
 	double stime;
 	double rtime;
@@ -29,44 +28,27 @@ struct times_val_t
 
 char *commands[100];
 
-void parseArgs (const int argc , char ** argv) 
-{
-	if (argc >= 2)
-	{
+void parseArgs (const int argc , char ** argv) {
+	if (argc >= 2) {
 		int i;
-		int flag = 0;
 		int j = 0;
-		for (i = 1; i < argc; ++i)
-		{
-			if (argv[i][0] == '-' && !flag)
-			{	
-				fprintf(stderr, "Argument %s Not Supported\n" , argv[i]);
-				continue;
-			}
-			else 
-			{
-				flag = 1;
-				commands[j++] = argv[i];
-			}
+		for (i = 1; i < argc; ++i) {
+			commands[j++] = argv[i];
 		}
 		commands[j++] = NULL;
-	}
-	else 
-	{
+	} else {
 		fprintf(stderr, "Insufficent Arguments\n");
 		exit(-1);
 	}
 }
 
-int runChild ()
-{
-	sighandler inturupt, quit;
-    int pid = fork();
+int runChild () {
+	int pid = fork();
     int status = 0;
     struct timeval start_time, end_time; // rtime
     struct rusage kernStat; // utime stime
-    if (pid != 0)
-    {
+    if (pid != 0) {
+    	sighandler inturupt, quit;
     	inturupt = signal(SIGINT , SIG_IGN);
         quit = signal(SIGQUIT , SIG_IGN);
         gettimeofday(&start_time , NULL); //No need for time-zones.
@@ -74,24 +56,19 @@ int runChild ()
 		gettimeofday(&end_time , NULL);
         signal(SIGINT , inturupt);
         signal(SIGQUIT , quit);
-        if ((status & 0xFF) == HARAKIRI) 
-        {
+        if ((status & 0xFF) == HARAKIRI) {
         	kill (pid , SIGKILL);
         	return HARAKIRI; // Because Kill Yourself.
         }
-        if ((status & 0xFF) != EXIT_SUCCESS) 
-        {
+        if ((status & 0xFF) != EXIT_SUCCESS) {
         	fprintf(stderr, "Child Killed By Code %d\n" , status);
         }
         times_val.stime = kernStat.ru_stime.tv_sec + (kernStat.ru_stime.tv_usec / 10e6); //s
         times_val.utime = kernStat.ru_stime.tv_sec + (kernStat.ru_utime.tv_usec / 10e6); //s 
         times_val.rtime = (end_time.tv_sec + (end_time.tv_usec / 10e6)) - (start_time.tv_sec + (start_time.tv_usec / 10e6));//us
         return 0;
-    }
-    else
-    {
- 		if (execvp ( commands[0] , commands ) < 0)
-		{
+    } else {
+ 		if (execvp ( commands[0] , commands ) < 0) {
             fprintf ( stderr , "COULD NOT FIND COMMAND : %s\n" , commands[0] );
 		    kill (getpid() , SIGTERM);
 		    return HARAKIRI;  // Because Kill Yourself.
@@ -100,17 +77,16 @@ int runChild ()
     return 0;
 }
 
-void printData() 
-{
+void printData() {
 	fprintf(stderr , "Real Time :%.3lf\n", times_val.rtime);
 	fprintf(stderr , "User Time :%.3lf\n", times_val.utime);
 	fprintf(stderr , "System Time :%.3lf\n", times_val.stime);
 }
 
-int main(const int argc, char ** argv)
-{
+int main(const int argc, char ** argv) {
 	parseArgs(argc, argv);
 	runChild();
 	printData();
 	return 0;
 }
+
